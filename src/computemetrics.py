1b99ba6c1fcfd1b21faa8587b5a4825429a9613b
Günther Kössler <Guenther.Koessler@aau.at>
2017-06-08 22:25:00 +0200

    computemetrics komplett; björnsgard delta und plot fehlen noch

--- src/computemetrics.py
+++ src/computemetrics.py
@@ -2,10 +2,10 @@ import sys
 import os
 import numpy
 import scipy.misc
+import pickle
+import shutil
 import PSNR
 import FileSplitter
-import PSNResults
-import pickle
 
 def listdir_nohidden(path):
     for f in os.listdir(path):
@@ -49,15 +49,19 @@ for videoFolder in videoFolders:
         h265Path = os.path.join(videoFolderPath, "h265")
 
         #Unkomprimiertes Video in PNG Frames splitten
-        rawFile = os.path.join(rawPath, listdir_nohidden(rawPath)[0])
+        for f in listdir_nohidden(rawPath):
+            tmp = os.path.join(rawPath,f)
+            if os.path.isfile(tmp):
+                rawFile = tmp
+                break
         FileSplitter.splitVideoIntoFrames(rawFile)
 
         bitrates = []
         psnrs = []
 
-        AV1files = listdir_nohidden(av1Path)
-        RAWFiles = listdir_nohidden(os.path.join(rawPath, "tmp"))
-        RAWFiles.sort()
+        AV1files = sorted(listdir_nohidden(av1Path))
+        H265files = sorted(listdir_nohidden(h265Path))
+        RAWFiles = sorted(listdir_nohidden(os.path.join(rawPath, "tmp")))
         for av1File in AV1files:
             bitrate = av1File.split("_")[0]
             if(bitrate.isdigit()):
@@ -72,8 +76,7 @@ for videoFolder in videoFolders:
                     FileSplitter.splitVideoIntoFrames(os.path.join(av1Path,av1File))
                     break
             # compute psnr && remove tmp folder for av1
-            AV1Tmpfiles = listdir_nohidden(os.path.join(av1Path, "tmp"))
-            AV1Tmpfiles.sort()
+            AV1Tmpfiles = sorted(listdir_nohidden(os.path.join(av1Path, "tmp")))
 
             for i in range(1, len(RAWFiles)):
                 imageRaw = scipy.misc.imread(os.path.join(rawPath, "tmp", RAWFiles[i]), flatten=True).astype(numpy.float32)
@@ -81,16 +84,55 @@ for videoFolder in videoFolders:
                 psnrs.append(PSNR.psnr(imageRaw, imageEnc))
 
             meanpsnr = numpy.mean(psnrs)
-            result = PSNResults(meanpsnr, bitrate)
+            # result = PSNResults(meanpsnr, bitrate)
+            result = [{"bitrate" : bitrate,"meanpsnr" : meanpsnr}]
 
             #Output File erzeugen bzw. erweitern
-            if os.path.isfile(os.path.join(videoOutputPath, "psnr")):
-                print("lade dieses File")
+            outFile = os.path.join(videoOutputPath, "av1_psnr.pickle")
+            if not os.path.isfile(outFile):
+                with open(outFile, 'wb') as f:
+                    pickle.dump(result, f, pickle.HIGHEST_PROTOCOL)
             else:
-                pickle.dump(result)
+                with open(outFile, 'rb') as f:
+                    tmpresult = pickle.load(f)
+                    tmpresult.append(result)
+                with open(outFile, 'wb') as f:
+                    pickle.dump(tmpresult, f, pickle.HIGHEST_PROTOCOL)
 
-            print("AV1 mean psnr: " + str(meanpsnr) + "Bitrate: " + str(bitrate) + "Folder" + av1Path)
+            print("AV1 mean psnr: " + str(meanpsnr) + " - Bitrate: " + str(bitrate) + " - Folder" + av1Path)
+            shutil.rmtree(os.path.join(av1Path, "tmp"))
 
+            ####################################################################
+            for h265file in H265files:
+                if h265file.startswith(str(bitrate)):
+                    FileSplitter.splitVideoIntoFrames(os.path.join(h265Path,h265file))
+                    break
+            # compute psnr && remove tmp folder for av1
+            H265Tmpfiles = sorted(listdir_nohidden(os.path.join(h265Path, "tmp")))
+
+            for i in range(1, len(RAWFiles)):
+                imageRaw = scipy.misc.imread(os.path.join(rawPath, "tmp", RAWFiles[i]), flatten=True).astype(numpy.float32)
+                imageEnc = scipy.misc.imread(os.path.join(h265Path, "tmp", H265Tmpfiles[i]), flatten=True).astype(numpy.float32)
+                psnrs.append(PSNR.psnr(imageRaw, imageEnc))
+
+            meanpsnr = numpy.mean(psnrs)
+            # result = PSNResults(meanpsnr, bitrate)
+            result = [{"bitrate" : bitrate,"meanpsnr" : meanpsnr}]
+
+            #Output File erzeugen bzw. erweitern
+            outFile = os.path.join(videoOutputPath, "h265_psnr.pickle")
+            if not os.path.isfile(outFile):
+                with open(outFile, 'wb') as f:
+                    pickle.dump(result, f, pickle.HIGHEST_PROTOCOL)
+            else:
+                with open(outFile, 'rb') as f:
+                    tmpresult = pickle.load(f)
+                    tmpresult.append(result)
+                with open(outFile, 'wb') as f:
+                    pickle.dump(tmpresult, f, pickle.HIGHEST_PROTOCOL)
+
+            print("H265 mean psnr: " + str(meanpsnr) + " - Bitrate: " + str(bitrate) + " - Folder" + h265Path)
+            shutil.rmtree(os.path.join(h265Path, "tmp"))
 
 print("test")
 #image1 = scipy.misc.imread(image_ref1, flatten=True).astype(numpy.float32)
