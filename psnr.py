5edec2a024ceca0494a2fd353976f100f7fe02d3
miwutti <michael.wutti@aau.at>
2017-06-08 14:59:39 +0200

    PSNR methode erstellt

---
+++ psnr.py
@@ -0,0 +1,10 @@
+import numpy
+import math
+
+
+def psnr(image1, image2):
+    mse = numpy.mean((image1 - image2) ** 2)
+    if mse == 0:
+        return 100
+    max_value = 255.0
+    return 20 * math.log10(max_value / math.sqrt(mse))
