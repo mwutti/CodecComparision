e71021e73aaf14de8e5c0ce3ad58fd82d522bc8c
miwutti <michael.wutti@aau.at>
2017-06-08 21:08:26 +0200

    OMFG

---
+++ src/PSNResults.py
@@ -0,0 +1,4 @@
+class ClassName:
+    def __init__(self, psnr, bitrate):
+        self.psnr = psnr
+        self.bitrate = bitrate
--- src/computemetrics.py
+++ src/computemetrics.py
@@ -4,6 +4,13 @@ import numpy
 import scipy.misc
 import PSNR
 import FileSplitter
+import PSNResults
+import pickle
+
+def listdir_nohidden(path):
+    for f in os.listdir(path):
+        if not f.startswith('.'):
+            yield f
 
 if len(sys.argv) != 3:
     print("must have 2 arguments: #" + str(len(sys.argv) - 1))
@@ -15,48 +22,75 @@ output_folder = args[2]
 working_directory = os.path.dirname(os.path.abspath(__file__))
 bitrates = []
 
-# FileSplitter.splitVideoIntoFrames();
+#Ordner fuer die Videos
+videoFolders = listdir_nohidden(os.path.join(working_directory, input_folder))
 
-videoFolders = os.listdir(os.path.join(working_directory, input_folder))
+outputPath = os.path.join(working_directory, output_folder)
 
+#OutputFolder erzeugen
+if not os.path.exists(outputPath):
+    os.makedirs(outputPath)
+
+#Metriken fuer alle Videos berechnen
 for videoFolder in videoFolders:
-    videoFolderPath = os.path.join(working_directory, input_folder, videoFolder)
-    rawPath = os.path.join(videoFolderPath, "raw")
-    av1Path = os.path.join(videoFolderPath, "av1")
-    h265Path = os.path.join(videoFolderPath, "h265")
-    print(videoFolder)
+    if not videoFolder.startswith("."):
+        videoFolderPath = os.path.join(working_directory, input_folder, videoFolder)
 
-    rawFile = os.path.join(rawPath,os.listdir(rawPath)[0])
-    FileSplitter.splitVideoIntoFrames(rawFile)
+        # Subfolder in Outputfolder erstellen
+        videoOutputPath = os.path.join(outputPath, videoFolder)
 
-    bitrates = []
-    psnrs = []
+        if not os.path.exists(videoOutputPath):
+            os.makedirs(videoOutputPath)
 
-    AV1files = os.listdir(av1Path)
-    RAWFiles = os.listdir(os.path.join(rawPath, "tmp"))
-    RAWFiles.sort()
-    for av1File in AV1files:
-        bitrate = av1File.split("_")[0]
-        if(bitrate.isdigit()):
-            bitrates.append(int(bitrate))
 
-    bitrates.sort()
+        #Pfade fuer die src Dateien
+        rawPath = os.path.join(videoFolderPath, "raw")
+        av1Path = os.path.join(videoFolderPath, "av1")
+        h265Path = os.path.join(videoFolderPath, "h265")
 
-    # split vids
-    for bitrate in bitrates:
+        #Unkomprimiertes Video in PNG Frames splitten
+        rawFile = os.path.join(rawPath, listdir_nohidden(rawPath)[0])
+        FileSplitter.splitVideoIntoFrames(rawFile)
+
+        bitrates = []
+        psnrs = []
+
+        AV1files = listdir_nohidden(av1Path)
+        RAWFiles = listdir_nohidden(os.path.join(rawPath, "tmp"))
+        RAWFiles.sort()
         for av1File in AV1files:
-            if av1File.startswith(str(bitrate)):
-                FileSplitter.splitVideoIntoFrames(os.path.join(av1Path,av1File))
-                break
-        # compute psnr && remove tmp folder for av1
-        AV1Tmpfiles = os.listdir(os.path.join(av1Path, "tmp"))
-        AV1Tmpfiles.sort()
-        for i in range(1, len(RAWFiles)):
-            imageRaw = scipy.misc.imread(os.path.join(rawPath, "tmp", RAWFiles[i]), flatten=True).astype(numpy.float32)
-            imageEnc = scipy.misc.imread(os.path.join(av1Path, "tmp", AV1Tmpfiles[i]), flatten=True).astype(numpy.float32)
-            psnrs[i] = PSNR.psnr(imageRaw, imageEnc)
-        meanpsnr = numpy.mean(psnrs)
-        print("AV1 mean psnr: " + str(meanpsnr))
+            bitrate = av1File.split("_")[0]
+            if(bitrate.isdigit()):
+                bitrates.append(int(bitrate))
+
+        bitrates.sort()
+
+        # split vids
+        for bitrate in bitrates:
+            for av1File in AV1files:
+                if av1File.startswith(str(bitrate)):
+                    FileSplitter.splitVideoIntoFrames(os.path.join(av1Path,av1File))
+                    break
+            # compute psnr && remove tmp folder for av1
+            AV1Tmpfiles = listdir_nohidden(os.path.join(av1Path, "tmp"))
+            AV1Tmpfiles.sort()
+
+            for i in range(1, len(RAWFiles)):
+                imageRaw = scipy.misc.imread(os.path.join(rawPath, "tmp", RAWFiles[i]), flatten=True).astype(numpy.float32)
+                imageEnc = scipy.misc.imread(os.path.join(av1Path, "tmp", AV1Tmpfiles[i]), flatten=True).astype(numpy.float32)
+                psnrs.append(PSNR.psnr(imageRaw, imageEnc))
+
+            meanpsnr = numpy.mean(psnrs)
+            result = PSNResults(meanpsnr, bitrate)
+
+            #Output File erzeugen bzw. erweitern
+            if os.path.isfile(os.path.join(videoOutputPath, "psnr")):
+                print("lade dieses File")
+            else:
+                pickle.dump(result)
+
+            print("AV1 mean psnr: " + str(meanpsnr) + "Bitrate: " + str(bitrate) + "Folder" + av1Path)
+
 
 print("test")
 #image1 = scipy.misc.imread(image_ref1, flatten=True).astype(numpy.float32)
