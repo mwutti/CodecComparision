651090d24483f7f292c7c03253f79567108222c9
miwutti <michael.wutti@aau.at>
2017-06-08 17:35:38 +0200

    video Folders in Variablen

--- src/computemetrics.py
+++ src/computemetrics.py
@@ -20,6 +20,10 @@ working_directory = os.path.dirname(os.path.abspath(__file__))
 videoFolders = os.listdir(os.path.join(working_directory, input_folder))
 
 for videoFolder in videoFolders:
+    videoFolderPath = os.path.join(working_directory, input_folder, videoFolder)
+    rawPath = os.path.join(videoFolderPath, "raw")
+    av1Path = os.path.join(videoFolderPath, "av1")
+    h265Path = os.path.join(videoFolderPath, "h265")
     print(videoFolder)
 
 
