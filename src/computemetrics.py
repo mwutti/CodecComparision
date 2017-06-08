5536a580c16e7f0874f07beed29d6875733a4f92
miwutti <michael.wutti@aau.at>
2017-06-08 17:20:06 +0200

    asd

--- src/computemetrics.py
+++ src/computemetrics.py
@@ -17,7 +17,6 @@ working_directory = os.path.dirname(os.path.abspath(__file__))
 image_ref1 = 'image1.jpg'
 image_ref2 = 'image2.jpg'
 
-
 image1 = scipy.misc.imread(image_ref1, flatten=True).astype(numpy.float32)
 image2 = scipy.misc.imread(image_ref2, flatten=True).astype(numpy.float32)
 
